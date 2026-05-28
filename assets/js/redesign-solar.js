f2be5afd919b986d609ae361b3d8e4de6d1e45a2
Camila Portela <camila.portela@estudante.ufjf.br>
2026-05-28 18:15:10 -0300

    Oculta orbitas abaixo da seta da galeria

--- assets/js/redesign-solar.js
+++ assets/js/redesign-solar.js
@@ -52,6 +52,8 @@
     solarRoot.position.y = -8;
     scene.add(solarRoot);
 
+    var orbitMeshes = [];
+
     var textureLoader = new THREE.TextureLoader();
     var PLANET_TEXTURE_PATH = "assets/img/planets/";
 
@@ -577,6 +579,7 @@
         var orbit = new THREE.Mesh(orbitGeometry, orbitMaterial);
         orbit.rotation.x = -0.5 * Math.PI;
         solarRoot.add(orbit);
+        orbitMeshes.push(orbit);
       }
       solarRoot.add(obj);
 
@@ -781,6 +784,69 @@
       solarRoot.position.y = -8;
     }
 
+    function getOrbitCutoffY() {
+      var galleryArrow = document.querySelector('.home-posts-gallery__arrow--next') || document.querySelector('.home-posts-gallery__arrow');
+      if (!galleryArrow) {
+        return null;
+      }
+
+      return galleryArrow.getBoundingClientRect().top - 4;
+    }
+
+    function updateOrbitVisibility() {
+      var cutoffY = getOrbitCutoffY();
+      if (cutoffY === null) {
+        return;
+      }
+
+      var cameraMatrix = camera.matrixWorldInverse;
+      var projectionMatrix = camera.projectionMatrix;
+      var box = new THREE.Box3();
+      var corners = [
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3()
+      ];
+
+      for (var i = 0; i < orbitMeshes.length; i += 1) {
+        var orbitMesh = orbitMeshes[i];
+        orbitMesh.updateMatrixWorld(true);
+        orbitMesh.geometry.computeBoundingBox();
+        box.copy(orbitMesh.geometry.boundingBox).applyMatrix4(orbitMesh.matrixWorld);
+
+        corners[0].set(box.min.x, box.min.y, box.min.z);
+        corners[1].set(box.min.x, box.min.y, box.max.z);
+        corners[2].set(box.min.x, box.max.y, box.min.z);
+        corners[3].set(box.min.x, box.max.y, box.max.z);
+        corners[4].set(box.max.x, box.min.y, box.min.z);
+        corners[5].set(box.max.x, box.min.y, box.max.z);
+        corners[6].set(box.max.x, box.max.y, box.min.z);
+        corners[7].set(box.max.x, box.max.y, box.max.z);
+
+        var maxScreenY = -Infinity;
+
+        for (var j = 0; j < corners.length; j += 1) {
+          var corner = corners[j].clone().applyMatrix4(cameraMatrix).applyMatrix4(projectionMatrix);
+          var screenY = ((1 - corner.y) * 0.5) * window.innerHeight;
+          if (screenY > maxScreenY) {
+            maxScreenY = screenY;
+          }
+        }
+
+        var shouldHide = maxScreenY >= cutoffY;
+        orbitMesh.visible = !shouldHide;
+        if (orbitMesh.material) {
+          orbitMesh.material.opacity = shouldHide ? 0 : 0.12;
+          orbitMesh.material.needsUpdate = true;
+        }
+      }
+    }
+
     function animate() {
       requestAnimationFrame(animate);
 
@@ -794,6 +860,7 @@
       moonObj.rotateY(0.03 * SOLAR_SPEED);
 
       controls.update();
+      updateOrbitVisibility();
       renderer.render(scene, camera);
     }
 
