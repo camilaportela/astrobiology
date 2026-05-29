3ab2d29bdb260d3f48f1fa999d7285ebcfc56bfa
Camila Portela <camila.portela@estudante.ufjf.br>
2026-05-29 14:11:03 -0300

    DNA: refina posição interna na faixa direita

--- assets/js/redesign-dna.js
+++ assets/js/redesign-dna.js
@@ -157,7 +157,7 @@
     var curve1 = new SinCurve1(CURVE_SCALE);
     var dna = new DNA(curve1, TOTAL_RUNGS);
     // posiciona no corredor direito sem encostar no limite externo
-    dna.position.set(0, DNA_POS_Y, 16);
+    dna.position.set(-3, DNA_POS_Y, 16);
     dna.rotation.y = -0.25;
     dna.scale.setScalar(DNA_SCALE);
     scene.add(dna);
@@ -171,7 +171,7 @@
       camera.updateProjectionMatrix();
       renderer.setSize(width, height, false);
 
-      dna.position.x = 0;
+      dna.position.x = -3;
       dna.position.y = DNA_POS_Y;
     }
 
